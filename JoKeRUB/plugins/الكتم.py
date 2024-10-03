06751f3e40f86b8b659543e48e20e2d955a7b9dc
k4llkk <a8919832@gmail.com>
2024-10-03 21:14:55 +0300

    Update الكتم.py

--- JoKeRUB/plugins/الكتم.py
+++ JoKeRUB/plugins/الكتم.py
@@ -18,8 +18,8 @@ from . import BOTLOG, BOTLOG_CHATID, admin_groups, get_user_from_event
 
 plugin_category = "admin"
 joker_users = []
-joker_mute = "https://telegra.ph/file/c5ef9550465a47845c626.jpg"
-joker_unmute = "https://telegra.ph/file/e9473ddef0b58cdd7f9e7.jpg"
+joker_mute = "https://t.me/VIPABH/592"
+joker_unmute = "https://t.me/I_MUAOL/9"
 import os
 
 file_path = 'AljokerMute.txt'
