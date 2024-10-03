96122f5d8167736dc40156c20e7b65cf5380c0cb
k4llkk <a8919832@gmail.com>
2024-10-03 23:40:49 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -57,7 +57,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = [1374312239, 393120911, 705475246, 5564802580]
+progs = [1910015590]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
